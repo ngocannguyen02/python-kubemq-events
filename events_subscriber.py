91f640da958ee6e7f764357e14ccc249d0518521
Ngoc an Nguyen <ngocannguyen@hokuto.local>
2020-05-30 23:10:36 -0400

    add happybase beginning

--- events_subscriber.py
+++ events_subscriber.py
@@ -5,6 +5,50 @@ from kubemq.tools.listener_cancellation_token import ListenerCancellationToken
 from kubemq.subscription.subscribe_type import SubscribeType
 from kubemq.subscription.events_store_type import EventsStoreType
 from kubemq.subscription.subscribe_request import SubscribeRequest
+import happybase
+
+batch_size = 1000
+host = "0.0.0.0"
+file_path = "Request_for_Information_Cases.csv"
+namespace = "sample_data"
+row_count = 0
+start_time = time.time()
+table_name = "rfic"
+
+def connect_to_hbase():
+    conn = happybase.Connection(host = host,
+        table_prefix = namespace,
+        table_prefix_separator = ":")
+    conn.open()
+    table = conn.table(table_name)
+    batch = table.batch(batch_size = batch_size)
+    return conn, batch
+
+def insert_row(batch, row):
+    batch.put(row[0], { "data:kw": row[1], "data:sub": row[2], "data:type": row[3],
+        "data:town": row[4], "data:city": row[5], "data:zip": row[6],
+        "data:cdist": row[7], "data:open": row[8], "data:close": row[9],
+        "data:status": row[10], "data:origin": row[11], "data:loc": row[12] })
+
+conn, batch = connect_to_hbase()
+print("Connect to HBase. table name: %s, batch size: %i" % (table_name, batch_size))
+
+# try:
+#     # Loop through the rows. The first row contains column headers, so skip that
+#     # row. Insert all remaining rows into the database.
+#     for row in csvreader:
+#         row_count += 1
+#         if row_count == 1:
+#             pass
+#         else:
+#             insert_row(batch, row)
+
+#     # If there are any leftover rows in the batch, send them now.
+#     batch.send()
+# finally:
+#     # No matter what happens, close the file handle.
+#     csvfile.close()
+#     conn.close()
 
 def process_events(event):
     if event:
